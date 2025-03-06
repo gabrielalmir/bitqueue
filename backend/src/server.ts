2484abc7b5344e094bf6649baafe08f28d4083a8
gabrielalmir <gabr.almir@gmail.com>
2025-03-06 20:23:00 -0300

    feat: integrate Fastify and Zod for improved server setup and environment configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,14 @@
-import { Database } from 'bun:sqlite';
+import { app } from "./app";
+import { env } from "./config/env";
 
-const db = new Database("data/bitqueue.db");
-const query = db.query("select 'BitQueue!' as message");
+async function main() {
+    try {
+        await app.listen({ port: env.PORT, host: '0.0.0.0' });
+        console.log(`Server running at http://localhost:${env.PORT}`);
+    } catch (error) {
+        app.log.error(error);
+        process.exit(1);
+    }
+}
 
-console.log(query.get())
+main();
